691bdf1163d044412f47ed4d6a0b42fad33ce3a5
thomasmodesitt82-dot <thomas.modesitt82@gmail.com>
2026-05-28 12:50:12 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Call screening app is running.");
+});
+
+app.post("/incoming-call", (req, res) => {
+
+  console.log("Incoming call received:");
+
+  console.log(req.body);
+
+  res.json({
+    message: "Webhook received"
+  });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server running on port ${PORT}`);
+
+});
