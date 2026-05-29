1c35b9eaa04243c230080b30dccbea4e2a4d4246
thomasmodesitt82-dot <thomas.modesitt82@gmail.com>
2026-05-29 13:19:11 -0400

    server.js

--- server.js
+++ server.js
@@ -46,14 +46,15 @@ app.post("/incoming-call", async (req, res) => {
             Authorization: `Bearer ${process.env.TELNYX_API_KEY}`,
             "Content-Type": "application/json"
           },
-          body: JSON.stringify({
-            payload: "Thank you for calling. To help reduce spam and robocalls, please press 1 to connect.",
-            voice: "female",
-            language: "en-US",
-            valid_digits: "1",
-            maximum_digits: 1,
-            timeout_millis: 10000
-          })
+      body: JSON.stringify({
+  payload: "Thank you for calling. To help reduce spam and robocalls, please press 1 to connect.",
+  voice: "female",
+  language: "en-US",
+  valid_digits: "1",
+  maximum_digits: 1,
+  timeout_millis: 10000,
+  tries: 1
+})
         }
       );
 
